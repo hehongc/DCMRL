822113cda41c17df5a80685c16c344e19020ee1e
hehongc <153437120+hehongc@users.noreply.github.com>
2023-12-10 22:14:38 +0800

    Add files via upload

--- reproduce/kitchen/simpl_fine_tune.py
+++ reproduce/kitchen/simpl_fine_tune.py
@@ -1,5 +1,5 @@
 import gym
-import simpl.env.maze
+import simpl.env.kitchen
 from simpl.env.kitchen import KitchenTasks
 
 from .kitchen_vis import draw_kitchen
@@ -8,14 +8,12 @@ from .kitchen_vis import draw_kitchen
 env = gym.make('simpl-kitchen-v0')
 tasks = KitchenTasks.test_tasks
 config = dict(
-    policy=dict(hidden_dim=128, n_hidden=5),
-    qf=dict(hidden_dim=128, n_hidden=5),
-    n_qf=2,
-    constrained_sac=dict(auto_alpha=True, init_alpha=1,
-                         kl_clip=10, target_kl=5, alpha_lr=3e-3),
+    constrained_sac=dict(auto_alpha=True, kl_clip=20,
+                         target_kl=5, increasing_alpha=True),
     buffer_size=20000,
+    n_prior_episode=20,
     time_limit=280,
-    n_episode=200,
+    n_episode=1000,
     train=dict(batch_size=256, reuse_rate=256)
 )
 visualize_env = draw_kitchen
